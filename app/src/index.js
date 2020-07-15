b79b4c11c30f9d07f9912db1f3971d3351a6ea04
christiano39 <carneson39@gmail.com>
2020-07-14 21:39:26 -0600

    set up empty reducer

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
+import './index.css';
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
