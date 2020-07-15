13701e47b6f39c89ef42a320c4fdac4dbfc91519
christiano39 <carneson39@gmail.com>
2020-07-15 09:44:04 -0600

    styled app

--- app/src/components/Activity.js
+++ app/src/components/Activity.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 
 const Activity = props => {
+    const expense = () => {
+        const num = Math.round(props.activity.price * 5);
+        let priceArr = []
+        if (num === 0) {
+            return [];
+        }
+        for (let i = 1; i <= 5; i++){
+            if (i <= num) {
+                priceArr.push(1);
+            }else {
+                priceArr.push(0);
+            }
+        }
+        console.log(priceArr);
+        return priceArr;
+    }
+    
     return (
         <div>
             {props.error !== '' ? <span>{props.error}</span> : ''}
             {
                 props.activity ?
-                props.isFetching ? <h2>Loading...</h2> :
-                <p>Here's an idea! You should {props.activity.activity.toLowerCase()}</p>
+                props.isFetching ? <h3>Loading...</h3> :
+                <div className='activity'>
+                    <h2>Here's an idea!</h2>
+                    <p> You should {props.activity.activity.toLowerCase()}.</p>
+                    <p>{
+                        expense().map(item => {
+                            if (item === 1) {
+                                return <span key={uuid()} className='money colored'>$</span>
+                            }else {
+                                return <span key={uuid()} className='money greyed'>$</span>
+                            }
+                        })
+                    }</p>
+                </div>
                 : ''
             }
         </div>
--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -24,27 +24,29 @@ const Form = props => {
     }
 
     return (
-        <form onSubmit={getActivity}>
+        <div className='form'>
             <h2>I'M BORED!</h2>
-            <p>I need something to do! Preferably something</p>
-            <select
-                name='type'
-                value={formValues.type}
-                onChange={onInputChange}
-            >
-                <option value=''>that exists</option>
-                <option value='education'>educational</option>
-                <option value='recreational'>recreational</option>
-                <option value='social'>social</option>
-                <option value='diy'>DIY</option>
-                <option value='charity'>charitable</option>
-                <option value='cooking'>cooking-related</option>
-                <option value='relaxation'>relaxing</option>
-                <option value='music'>musical</option>
-                <option value='busywork'>to keep busy</option>
-            </select>
-            <button>Get activity</button>
-        </form>
+            <form onSubmit={getActivity}>
+                <p>I need something to do! Preferably something</p>&nbsp;
+                <select
+                    name='type'
+                    value={formValues.type}
+                    onChange={onInputChange}
+                >
+                    <option value=''>that exists</option>
+                    <option value='education'>educational</option>
+                    <option value='recreational'>recreational</option>
+                    <option value='social'>social</option>
+                    <option value='diy'>DIY</option>
+                    <option value='charity'>charitable</option>
+                    <option value='cooking'>cooking-related</option>
+                    <option value='relaxation'>relaxing</option>
+                    <option value='music'>musical</option>
+                    <option value='busywork'>to keep busy</option>
+                </select>
+                <button>Get activity</button>
+            </form>
+        </div>
     )
 }
 
