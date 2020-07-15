1c71a905f5f41fbb108b3f259b8bc678d7f3be03
christiano39 <carneson39@gmail.com>
2020-07-14 23:16:27 -0600

    added form to control axios request

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -4,10 +4,10 @@ export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAIL = "FETCH_FAIL";
 
-export const getActivity = () => dispatch => {
+export const getActivity = url => dispatch => {
     dispatch({ type: FETCH_START });
     axios
-        .get('http://www.boredapi.com/api/activity/')
+        .get(url)
         .then(res => {
             console.log(res);
             dispatch({ type: FETCH_SUCCESS, payload: res.data });
--- app/src/components/Form.js
+++ app/src/components/Form.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { getActivity } from '../actions'
 
+const initialFormValues = {
+    type: ''
+}
+
 const Form = props => {
+    const [formValues, setFormValues] = useState(initialFormValues);
+
+    const onInputChange = e => {
+        const { name, value } = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    }
+    
+    const getActivity = e => {
+        e.preventDefault();
+        props.getActivity(`http://www.boredapi.com/api/activity?type=${formValues.type}`);
+    }
+
     return (
-        <button onClick={props.getActivity}>Get activity</button>
+        <form onSubmit={getActivity}>
+            <h2>I'M BORED!</h2>
+            <p>I need something to do! Preferably something</p>
+            <select
+                name='type'
+                value={formValues.type}
+                onChange={onInputChange}
+            >
+                <option value=''>that exists</option>
+                <option value='education'>educational</option>
+                <option value='recreational'>recreational</option>
+                <option value='social'>social</option>
+                <option value='diy'>DIY</option>
+                <option value='charity'>charitable</option>
+                <option value='cooking'>cooking-related</option>
+                <option value='relaxation'>relaxing</option>
+                <option value='music'>musical</option>
+                <option value='busywork'>to keep busy</option>
+            </select>
+            <button>Get activity</button>
+        </form>
     )
 }
 
